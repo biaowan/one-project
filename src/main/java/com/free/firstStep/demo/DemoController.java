fa0d12c06e1a4d12b850f08e8526eaafe34933bb
MrWb <wb@.com>
2023-03-27 10:29:14 +0800

    方法提交

--- src/main/java/com/free/firstStep/demo/DemoController.java
+++ src/main/java/com/free/firstStep/demo/DemoController.java
@@ -2,4 +2,13 @@ package com.free.firstStep.demo;
 
 public class DemoController {
 
+    /**
+     * @desc: demo测试
+     * @auther: wb
+     * @date: 2023-03-27 10:26
+     */
+    public void oneFreeDemo() {
+
+    }
+
 }
