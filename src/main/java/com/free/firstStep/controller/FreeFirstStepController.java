30ee8e1771c248ad6a03bb12214c55176a1b8cd0
MrWb <wb@.com>
2023-03-27 11:45:11 +0800

    李四提交的代码

--- src/main/java/com/free/firstStep/controller/FreeFirstStepController.java
+++ src/main/java/com/free/firstStep/controller/FreeFirstStepController.java
@@ -1,7 +1,17 @@
 package com.free.firstStep.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class FreeFirstStepController {
 
-    //修改
+    /**
+     * @desc: 模拟代码冲突
+     * @auther: 李四
+     * @date: 2023-03-27 10:56
+     */
+    @PostMapping("/first/demo2")
+    public void firstDemo1(){
+        System.out.println("李四写的代码........");
+    }
 
 }
